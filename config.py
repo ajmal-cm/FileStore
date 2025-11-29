ea9c07674f5d2c7fdb32d97cb79518201d34c56d
ajmal-cm <thafiyaparveen47@gmail.com>
2025-11-29 16:37:09 +0530

    Update database configuration with new URI and name

--- config.py
+++ config.py
@@ -19,8 +19,8 @@ API_ID = ""
 API_HASH = ""
 WORKERS = 5
 
-DB_URI = "mongodb"
-DB_NAME = "yato"
+DB_URI = "mongodb+srv://acoolvoice03_db_user:mE571rr4kjkPUTkW@cluster0.t91uvkm.mongodb.net/"
+DB_NAME = "acoolvoice"
 
 FSUBS = [[-1003016571084, True, 10]] # Force Subscription Channels [channel_id, request_enabled, timer_in_minutes]
 # Database Channel (Primary)
